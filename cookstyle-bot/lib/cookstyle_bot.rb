200fe99a55359b8b8db580a37ddfe6ca8d0bfc1d
Dan Webb <dan.webb@damacus.io>
2025-05-19 16:37:08 +0100

    Don't log all settings
    Signed-off-by: Dan Webb <dan.webb@damacus.io>

--- cookstyle-bot/lib/cookstyle_bot.rb
+++ cookstyle-bot/lib/cookstyle_bot.rb
@@ -18,11 +18,37 @@ module CookstyleBot
     CookstyleBot::Logging.logger
   end
 
+  # Sanitize settings by redacting sensitive information before logging
+  # @return [Hash] Settings hash with sensitive values redacted
+  sig { returns(T::Hash[T.untyped, T.untyped]) }
+  private_class_method def self.sanitize_settings
+    settings_hash = Settings.to_h
+
+    # List of keys that might contain sensitive information
+    sensitive_keys = %w[token password key secret credential auth]
+
+    # Helper method to recursively sanitize nested hashes
+    sanitize = lambda do |hash|
+      hash.each_with_object({}) do |(k, v), result|
+        result[k] = if sensitive_keys.any? { |sensitive| k.to_s.downcase.include?(sensitive) }
+                      '[REDACTED]'
+                    elsif v.is_a?(Hash)
+                      sanitize.call(v)
+                    else
+                      v
+                    end
+      end
+    end
+
+    sanitize.call(settings_hash)
+  end
+
   sig { void }
   def self.run
     logger.info("CookstyleBot version #{VERSION} starting...")
-    logger.debug("Settings: #{Settings.to_h.inspect}")
-    # Main logic will be added here
+    logger.debug("Settings: #{sanitize_settings.inspect}")
+    runner = Runner.new
+    runner.run
     logger.info('CookstyleBot finished.')
   end
 end
